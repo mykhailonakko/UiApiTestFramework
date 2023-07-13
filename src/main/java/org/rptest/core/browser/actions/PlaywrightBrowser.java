eca60ad4d2d92c85bd2777ea0a2593eda32116ea
mykhailonakko <nakko.mihail@gmail.com>
2023-07-13 07:52:40 +0200

    Added an ability to take a screenshot on scenario fail
    added a lot of cucumber related configuration to the Junit platform properties file

--- src/main/java/org/rptest/core/browser/actions/PlaywrightBrowser.java
+++ src/main/java/org/rptest/core/browser/actions/PlaywrightBrowser.java
@@ -4,6 +4,7 @@ import com.microsoft.playwright.Page;
 import org.rptest.core.elements.PlaywrightUiElement;
 import org.rptest.core.elements.UiElement;
 
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -39,5 +40,11 @@ public class PlaywrightBrowser implements IBrowser {
         return page.locator(xpath).all().stream()
                 .map(PlaywrightUiElement::new)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public void takeScreenshot() {
+        page.screenshot((new Page.ScreenshotOptions()
+                .setPath(Paths.get("screenshot.png"))));
     }
 }
